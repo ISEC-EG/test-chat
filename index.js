a1af8ef78f4a7498e1ce4a811199cabca22b756b
Sherif Wahballa <dev1@isec.local>
2021-11-18 14:20:58 +0200

    f

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 // Dialogflow client APIs to make agents calls
 async function runDialogflowQuery(text, sessionId, language_code, credentials) {
   const project_id = credentials.project_id
-  const sessionClient = new dialogflow.SessionsClient({'credentials':credentials});
+  const sessionClient = new dialogflow.SessionsClient({ 'credentials': credentials });
   const sessionPath = sessionClient.sessionPath(project_id, sessionId);
   var request;
   request = {
@@ -50,81 +50,95 @@ app.post("/bot/:botid", (req, res) => {
   const botid = req.params.botid;
   const conversation = cbclient.supportRequest
   // immediately reply back
-  res.status(200).send({"success":true});
+  res.status(200).send({ "success": true });
   // reply messages are sent asynchronously
   const dialogflow_session_id = conversation.request_id
   const lang = 'en-EN' // lang must be the same of the Dialogflow Agent
   const credentials = JSON.parse(process.env[botid])
   runDialogflowQuery(cbclient.text, dialogflow_session_id, lang, credentials)
-  .then(function(result) {
-    console.log("query result: ", JSON.stringify(result))
-    console.log("is fallback:", result.intent.isFallback)
-    console.log("confidence:", result.intentDetectionConfidence)
-    // intentDetectionConfidence
+    .then(function (result) {
+      console.log("query result: ", JSON.stringify(result))
+      console.log("is fallback:", result.intent.isFallback)
+      console.log("confidence:", result.intentDetectionConfidence)
+      // intentDetectionConfidence
 
-    if(res.statusCode === 200) {
-      const reply_text = result['fulfillmentText']
-      var msg = {
-        "text": reply_text
-      }
-
-      cbclient.tiledeskClient.sendMessage(dialogflow_session_id, msg, function (err, response) {
-        if (err) {
-          console.log('Error: ', err)
+      if (res.statusCode === 200) {
+        const reply_text = result['fulfillmentText']
+        var msg = {
+          "text": reply_text
         }
-        // console.log('response', response);
-        console.log("Message sent.");
-      })
-    }
-  })
-  .catch(function(err) {
-    console.log('Error: ', err);
-  })
+
+        cbclient.tiledeskClient.sendMessage(dialogflow_session_id, msg, function (err, response) {
+          if (err) {
+            console.log('Error: ', err)
+          }
+          // console.log('response', response);
+          console.log("Message sent.");
+        })
+      }
+    })
+    .catch(function (err) {
+      console.log('Error: ', err);
+    })
 })
 
 // Tutorial 2 - Advanced tutorial using 'micro language' to render buttons or images
 app.post("/microlang-bot/:botid", (req, res) => {
-  const tdclient = new TiledeskChatbotClient({APIKEY:'__APIKEY__', request: req});
+  const tdclient = new TiledeskChatbotClient({ APIKEY: '__APIKEY__', request: req });
   const botid = req.params.botid;
   console.log("botid:", botid)
   const conversation = tdclient.supportRequest
   // immediately reply back
-  res.status(200).send({"success":true});
+  res.status(200).send({ "success": true });
   // reply messages are sent asynchronously
   const dialogflow_session_id = conversation.request_id
   const lang = 'en-EN' // lang must be the same of the Dialogflow Agent
   console.log("loading credentials...") //, process.env[botid])
   const credentials = JSON.parse(process.env[botid])
   runDialogflowQuery(tdclient.text, dialogflow_session_id, lang, credentials)
-  .then(function(result) {
-    // console.log("query result: ", JSON.stringify(result))
-    // console.log("is fallback:", result.intent.isFallback)
-    // console.log("confidence:", result.intentDetectionConfidence)
-    // intentDetectionConfidence
-    if(res.statusCode === 200) {
-      const reply_text = result['fulfillmentText'];
-      const parsed_reply = TiledeskChatbotUtil.parseReply(reply_text);
-      console.log('reply_text: ', parsed_reply)
-      var msg = parsed_reply.message;
-      // NOTE: you can also use parts of the parsed message, like this
-      // var reply = {
-      //   "text": msg.text,
-      //   "type": msg.type,
-      //   "attributes": msg.attributes,
-      //   "metadata": msg.metadata,
-      //   "senderFullname": tdclient.botName
-      // }
-      tdclient.sendMessage(msg, function (err, response) {
-        if (err) {
-          console.log('Error: ', err.message);
+    .then(function (result) {
+      // console.log("query result: ", JSON.stringify(result))
+      // console.log("is fallback:", result.intent.isFallback)
+      // console.log("confidence:", result.intentDetectionConfidence)
+      // intentDetectionConfidence
+      if (res.statusCode === 200) {
+        const reply_text = result['fulfillmentText'];
+        let reply = {
+          "message": {}
         }
-        console.log("Message", msg, "sent.");
-      })
-    }
-  })
-  .catch(function(err) {
-    console.log('Error: ', err);
-  })
+        reply.message[TiledeskChatbotUtil.TEXT_KEY] = text
+        reply.message[TiledeskChatbotUtil.TYPE_KEY] = TiledeskChatbotUtil.TYPE_TEXT
+
+        let parsed;
+        // parsed = TiledeskChatbotUtil.parse_legacy_image(text, reply);
+        parsed = TiledeskChatbotUtil.parse_tdImage(parsed.text, parsed.reply);
+        text = parsed.text;
+        reply = parsed.reply;
+
+
+        // const parsed_reply = TiledeskChatbotUtil.parseReply(reply_text);
+        console.log('reply_text: ', parsed_reply)
+        var msg = reply.message;
+        console.log('MSG: ', msg)
+        // NOTE: you can also use parts of the parsed message, like this
+        // var reply = {
+        //   "text": msg.text,
+        //   "type": msg.type,
+        //   "attributes": msg.attributes,
+        //   "metadata": msg.metadata,
+        //   "senderFullname": tdclient.botName
+        // }
+        tdclient.sendMessage(msg, function (err, response) {
+          if (err) {
+            console.log('Error: ', err.message);
+          }
+          console.log("Message", msg, "sent.");
+        })
+      }
+    })
+    .catch(function (err) {
+      console.log('Error: ', err);
+    })
 })
 
 // Tutorial 3 - Automatic human handhoff based on fallback intent
@@ -134,61 +148,62 @@ var consecutive_fallback_count = {};
 const MAX_FALLBACKS = 4;
 app.post("/bot-fallback-handoff/:botid", (req, res) => {
   const tdclient = new TiledeskChatbotClient(
-    {request: req,
-     APIURL: 'https://tiledesk-server-pre.herokuapp.com'
+    {
+      request: req,
+      APIURL: 'https://tiledesk-server-pre.herokuapp.com'
     });
   console.log("tdclient", tdclient)
   const botid = req.params.botid;
   const supportRequest = tdclient.supportRequest
   // immediately reply back
-  res.status(200).send({"success":true});
+  res.status(200).send({ "success": true });
   // reply messages are sent asynchronously
   const dialogflow_session_id = supportRequest.request_id
   const lang = 'en-EN' // lang must be the same of the Dialogflow Agent
   const credentials = JSON.parse(process.env[botid])
   runDialogflowQuery(tdclient.text, dialogflow_session_id, lang, credentials)
-  .then(function(result) {
-    if (!consecutive_fallback_count[dialogflow_session_id]) {
-      // init consecutive fallback count for this conversation
-      consecutive_fallback_count[dialogflow_session_id] = 0
-    }
-    if (result.intent.isFallback) {
-      consecutive_fallback_count[dialogflow_session_id]++
-      console.log("fallback of", dialogflow_session_id, "is", consecutive_fallback_count[dialogflow_session_id])
-    }
-    else {
-      // reset fallback on every positive hit.
-      // here you can also evaluate result.intentDetectionConfidence
-      // and consider it as a fallback if under some threshold value
-      consecutive_fallback_count[dialogflow_session_id] = 0
-    }
-    if(res.statusCode === 200) {
-      let msgs = [];
-      if (consecutive_fallback_count[dialogflow_session_id] == MAX_FALLBACKS) {
+    .then(function (result) {
+      if (!consecutive_fallback_count[dialogflow_session_id]) {
+        // init consecutive fallback count for this conversation
         consecutive_fallback_count[dialogflow_session_id] = 0
-        msgs.push({
-          "text": "I really don't understand your questions, putting you in touch with an operator..."
-        })
-        msgs.push({
-          "text": "\\agent"
-          // "attributes" : {subtype: "info"} // this message is hidden in the widget
-        })
+      }
+      if (result.intent.isFallback) {
+        consecutive_fallback_count[dialogflow_session_id]++
+        console.log("fallback of", dialogflow_session_id, "is", consecutive_fallback_count[dialogflow_session_id])
       }
       else {
-        msgs.push({
-          "text": result['fulfillmentText']
+        // reset fallback on every positive hit.
+        // here you can also evaluate result.intentDetectionConfidence
+        // and consider it as a fallback if under some threshold value
+        consecutive_fallback_count[dialogflow_session_id] = 0
+      }
+      if (res.statusCode === 200) {
+        let msgs = [];
+        if (consecutive_fallback_count[dialogflow_session_id] == MAX_FALLBACKS) {
+          consecutive_fallback_count[dialogflow_session_id] = 0
+          msgs.push({
+            "text": "I really don't understand your questions, putting you in touch with an operator..."
+          })
+          msgs.push({
+            "text": "\\agent"
+            // "attributes" : {subtype: "info"} // this message is hidden in the widget
+          })
+        }
+        else {
+          msgs.push({
+            "text": result['fulfillmentText']
+          })
+        }
+        msgs.forEach(m => {
+          tdclient.sendMessage(m, function (err) {
+            console.log("Message", m.text, "sent.");
+          })
         })
       }
-      msgs.forEach( m => {
-        tdclient.sendMessage(m, function (err) {
-          console.log("Message", m.text, "sent.");
-        })
-      })
-    }
-  })
-  .catch(function(err) {
-    console.log('Error: ', err);
-  })
+    })
+    .catch(function (err) {
+      console.log('Error: ', err);
+    })
 })
 
 // Tutorial 4 - Ask user for optional Agent handoff
@@ -206,7 +221,7 @@ app.post('/dfwebhook/:project_id', (req, res) => {
   if (intent === "TALK TO AGENT") {
     if (resbody && resbody.token) {
       const tdclient = new TiledeskClient()
-      tdclient.openNow(function(isopen) {
+      tdclient.openNow(function (isopen) {
         var df_res = {}
         if (isopen) {
           df_res['fulfillmentText'] = "We are open! Switching to agent\\agent"
